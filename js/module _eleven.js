7359a9511406bc441c3e8114af8bf70c9dfe7393
Vasek2024 <p.f1@yandex.ru>
2025-01-15 19:01:15 +0700

    поменял в задании 1 строку вивода сообщений

--- js/module _eleven.js
+++ js/module _eleven.js
@@ -1,4 +1,31 @@
 // Задание 1
+// можно ли применять такой синтаксис (сильно длинная строка вроде получилась)? (\n)
+// const tall = [1, 3, 0, 32, 15, 15, 9, 3, 196, 33, null];
+// function arrayNamber() {
+//   const  odd = tall.filter(x => typeof x === 'number' && x % 2 !== 0).length;
+// const  even = tall.filter(x => typeof x === 'number' && x % 2 === 0).length;
+// const  nolNumber = tall.filter(x => typeof x === 'number' && x === 0).length;
+// const  nulNumber = tall.filter(x => typeof x !== 'number' && x === null).length;
+
+// console.log(' не чётные - ' + odd + '\n чётные - ' + even + '\n нулей - ' + nolNumber + '\n значений null - ' + nulNumber);
+
+// const seen = new Set();
+// let hasDuplicates = false;
+// for (const num of tall) {
+//   if (seen.has(num)) {
+//     console.log(`Есть одинаковые значения (${num})`);
+//     hasDuplicates = true;
+//   } else {
+//     seen.add(num);
+//   }
+// }
+// if (!hasDuplicates) {
+//   console.log('Одинаковых значений нет');
+// }
+// };
+// arrayNamber(tall);
+
+
 // const tall = [1, 3, 0, 32, 10, 15, 9, 3, 196, 33, null];
 // function arrayNamber() {
 //   const  odd = tall.filter(x => typeof x === 'number' && x % 2 !== 0).length;
@@ -20,7 +47,7 @@
 //   }
 // }
 // if (!hasDuplicates) {
-  // console.log('Одинаковых значений нет');
+//   console.log('Одинаковых значений нет');
 // }
 // };
 // arrayNamber(6);
