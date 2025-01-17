ceeac0fb90fa2b04a21ffdc6059e6fa12a3d02af
Vasek2024 <p.f1@yandex.ru>
2025-01-17 14:42:40 +0700

    Учёл замечания ментора

--- js/task_four.js
+++ js/task_four.js
@@ -1,13 +1,30 @@
 // Задание 4
 const inter = function (a, b) {
-  for (let i = a; i < b + 1; i++) {
+  let i = a;
+  const intervalId = setInterval(() => {
     console.log(i);
-  }
-}
+    i++;
+    if (i > b) {
+      clearInterval(intervalId); 
+    }
+  }, 2000);
+};
 const btn = document.querySelector(".btnInterval");
 btn.addEventListener("click", () => {
-  const intervalId = setInterval(inter, 2000, 5, 15)
-  setTimeout(function () {
-    clearInterval(intervalId)
-  }, 2000)
+  inter(5, 15); 
 });
+
+
+// моё решение не верное
+// const inter = function (a, b) {
+//   for (let i = a; i < b + 1; i++) {
+//     console.log(i);
+//   }
+// }
+// const btn = document.querySelector(".btnInterval");
+// btn.addEventListener("click", () => {
+//   const intervalId = setInterval(inter, 2000, 5, 15)
+//   setTimeout(function () {
+//     clearInterval(intervalId)
+//   }, 2000)
+// });
--- js/task_three.js
+++ js/task_three.js
@@ -1,10 +1,16 @@
 // Задание 3
-function example(func) {
-  let num = 20;
-  let sumNum = num + argFunc();
-  return sumNum;
+function createAdder(num1) {
+  return function (num2) {
+    return num1 + num2;
+  };
 }
-function argFunc() {
-  return 2;
-}
-const resultFunc = example(argFunc);
+
+// function example(func) {
+//   let num = 20;
+//   let sumNum = num + argFunc();
+//   return sumNum;
+// }
+// function argFunc() {
+//   return 2;
+// }
+// const resultFunc = example(argFunc);
